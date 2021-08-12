023fefd31f6249c46303e6d1751388fd1e45af17
Ruli Sastra Putri <ruli@accelbyte.net>
2021-08-12 04:54:50 +0000

    feat: generate session browser sdk
    feat: add session browser factory
    
    feat: add session browser service wrapper
    
    fix: remove duplicate error checking in querySession
    
    feat: update querySession's parameter (matchId)
    
    Merged in feature/JSC-127-session-browser-service (pull request #47)
    
    Feature/JSC-127 session browser service
    
    Approved-by: Ahmad Nurjalal
    Approved-by: Mochamad Ridwan Taufik Hidayat
    Approved-by: Wijanarko Sukma Pamungkas

---
+++ services-api/pkg/factory/sessionbrowserclientfactory.go
@@ -0,0 +1,31 @@
+// Copyright (c) 2021 AccelByte Inc. All Rights Reserved.
+// This is licensed software from AccelByte Inc, for limitations
+// and restrictions contact your company contract manager.
+package factory
+
+import (
+	"github.com/AccelByte/accelbyte-go-sdk/services-api/pkg/repository"
+	"github.com/AccelByte/accelbyte-go-sdk/sessionbrowser-sdk/pkg/sessionbrowserclient"
+	"strings"
+)
+
+var sessionbrowserClientInstance *sessionbrowserclient.JusticeSessionbrowserService
+
+func NewSessionClient(configRepository repository.ConfigRepository) *sessionbrowserclient.JusticeSessionbrowserService {
+	if sessionbrowserClientInstance == nil {
+		baseUrl := configRepository.GetJusticeBaseUrl()
+		if len(baseUrl) > 0 {
+			baseUrlSplit := strings.Split(baseUrl, "://")
+			httpClientConfig := &sessionbrowserclient.TransportConfig{
+				Host:     baseUrlSplit[1],
+				BasePath: "/sessionbrowser",
+				Schemes:  []string{baseUrlSplit[0]},
+			}
+			sessionbrowserClientInstance = sessionbrowserclient.NewHTTPClientWithConfig(nil, httpClientConfig)
+		} else {
+			sessionbrowserClientInstance = sessionbrowserclient.NewHTTPClient(nil)
+		}
+	}
+
+	return sessionbrowserClientInstance
+}

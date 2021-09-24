9b1878e507e287fd0a5fd9549f9d0452f00a6968
Ahmad Nurjalal <ahmad.nurjalal@accelbyte.net>
2021-09-24 08:56:54 +0700

    feat: add event log client factory

---
+++ services-api/pkg/factory/eventlogclientfactory.go
@@ -0,0 +1,28 @@
+package factory
+
+import (
+	"github.com/AccelByte/accelbyte-go-sdk/eventlog-sdk/pkg/eventlogclient"
+	"strings"
+
+	"github.com/AccelByte/accelbyte-go-sdk/services-api/pkg/repository"
+)
+
+var eventlogClientInstance *eventlogclient.JusticeEventlogService
+
+func NewEventLogClient(configRepository repository.ConfigRepository) *eventlogclient.JusticeEventlogService {
+	if eventlogClientInstance == nil {
+		baseUrl := configRepository.GetJusticeBaseUrl()
+		if len(baseUrl) > 0 {
+			baseUrlSplit := strings.Split(baseUrl, "://")
+			httpClientConfig := &eventlogclient.TransportConfig{
+				Host:     baseUrlSplit[1],
+				BasePath: "",
+				Schemes:  []string{baseUrlSplit[0]},
+			}
+			eventlogClientInstance = eventlogclient.NewHTTPClientWithConfig(nil, httpClientConfig)
+		} else {
+			eventlogClientInstance = eventlogclient.NewHTTPClient(nil)
+		}
+	}
+	return eventlogClientInstance
+}

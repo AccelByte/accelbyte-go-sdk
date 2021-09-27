33d1e2415c4eeed86dc7dc14b85328709b94d9d0
ruli <ruli@accelbyte.net>
2021-09-27 17:58:00 +0700

    feat: the complete wrappers for legal service

---
+++ services-api/pkg/factory/legalclientfactory.go
@@ -0,0 +1,32 @@
+// Copyright (c) 2021 AccelByte Inc. All Rights Reserved.
+// This is licensed software from AccelByte Inc, for limitations
+// and restrictions contact your company contract manager.
+package factory
+
+import (
+	"strings"
+
+	"github.com/AccelByte/accelbyte-go-sdk/legal-sdk/pkg/legalclient"
+	"github.com/AccelByte/accelbyte-go-sdk/services-api/pkg/repository"
+)
+
+var legalClientInstance *legalclient.JusticeLegalService
+
+func NewLegalClient(configRepository repository.ConfigRepository) *legalclient.JusticeLegalService {
+	if legalClientInstance == nil {
+		baseUrl := configRepository.GetJusticeBaseUrl()
+		if len(baseUrl) > 0 {
+			baseUrlSplit := strings.Split(baseUrl, "://")
+			httpClientConfig := &legalclient.TransportConfig{
+				Host:     baseUrlSplit[1],
+				BasePath: "",
+				Schemes:  []string{baseUrlSplit[0]},
+			}
+			legalClientInstance = legalclient.NewHTTPClientWithConfig(nil, httpClientConfig)
+		} else {
+			legalClientInstance = legalclient.NewHTTPClient(nil)
+		}
+	}
+
+	return legalClientInstance
+}

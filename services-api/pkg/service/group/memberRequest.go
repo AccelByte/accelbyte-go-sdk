2769883c1a691076616807bf7349163046eac2c6
Ahmad Nurjalal <ahmad.nurjalal@accelbyte.net>
2021-10-14 16:23:48 +0700

    feat: add memberRequest wrapper to group

---
+++ services-api/pkg/service/group/memberRequest.go
@@ -0,0 +1,83 @@
+package group
+
+import (
+	"encoding/json"
+	"github.com/AccelByte/accelbyte-go-sdk/group-sdk/pkg/groupclient"
+	"github.com/AccelByte/accelbyte-go-sdk/group-sdk/pkg/groupclient/member_request"
+	"github.com/AccelByte/accelbyte-go-sdk/group-sdk/pkg/groupclientmodels"
+	"github.com/AccelByte/accelbyte-go-sdk/services-api/pkg/repository"
+	"github.com/go-openapi/runtime/client"
+	"github.com/sirupsen/logrus"
+)
+
+type MemberRequestService struct {
+	Client          *groupclient.JusticeGroupService
+	TokenRepository repository.TokenRepository
+}
+
+func (m *MemberRequestService) CreateMemberRoleAdminV1(input *member_request.GetGroupInvitationRequestPublicV1Params) (*groupclientmodels.ModelsGetMemberRequestsListResponseV1, error) {
+	token, err := m.TokenRepository.GetToken()
+	if err != nil {
+		logrus.Error(err)
+		return nil, err
+	}
+	created, badRequest, unauthorized, forbidden, internalServerError, err := m.Client.MemberRequest.GetGroupInvitationRequestPublicV1(input, client.BearerToken(*token.AccessToken))
+	if badRequest != nil {
+		errorMsg, _ := json.Marshal(*badRequest.GetPayload())
+		logrus.Error(string(errorMsg))
+		return nil, badRequest
+	}
+	if unauthorized != nil {
+		errorMsg, _ := json.Marshal(*unauthorized.GetPayload())
+		logrus.Error(string(errorMsg))
+		return nil, unauthorized
+	}
+	if forbidden != nil {
+		errorMsg, _ := json.Marshal(*forbidden.GetPayload())
+		logrus.Error(string(errorMsg))
+		return nil, forbidden
+	}
+	if internalServerError != nil {
+		errorMsg, _ := json.Marshal(*internalServerError.GetPayload())
+		logrus.Error(string(errorMsg))
+		return nil, internalServerError
+	}
+	if err != nil {
+		logrus.Error(err)
+		return nil, err
+	}
+	return created.GetPayload(), nil
+}
+func (m *MemberRequestService) GetGroupJoinRequestPublicV1(input *member_request.GetGroupJoinRequestPublicV1Params) (*groupclientmodels.ModelsGetMemberRequestsListResponseV1, error) {
+	token, err := m.TokenRepository.GetToken()
+	if err != nil {
+		logrus.Error(err)
+		return nil, err
+	}
+	created, badRequest, unauthorized, forbidden, internalServerError, err := m.Client.MemberRequest.GetGroupJoinRequestPublicV1(input, client.BearerToken(*token.AccessToken))
+	if badRequest != nil {
+		errorMsg, _ := json.Marshal(*badRequest.GetPayload())
+		logrus.Error(string(errorMsg))
+		return nil, badRequest
+	}
+	if unauthorized != nil {
+		errorMsg, _ := json.Marshal(*unauthorized.GetPayload())
+		logrus.Error(string(errorMsg))
+		return nil, unauthorized
+	}
+	if forbidden != nil {
+		errorMsg, _ := json.Marshal(*forbidden.GetPayload())
+		logrus.Error(string(errorMsg))
+		return nil, forbidden
+	}
+	if internalServerError != nil {
+		errorMsg, _ := json.Marshal(*internalServerError.GetPayload())
+		logrus.Error(string(errorMsg))
+		return nil, internalServerError
+	}
+	if err != nil {
+		logrus.Error(err)
+		return nil, err
+	}
+	return created.GetPayload(), nil
+}

e57e3496f0f48361172a4b7f0cffb013b8f22e62
Elmer Nocon <elmernocon@gmail.com>
2022-06-03 12:07:26 +0800

    fix(cli): change loginClient command to use LoginClient wrapper

--- samples/cli/cmd/loginClient.go
+++ samples/cli/cmd/loginClient.go
@@ -13,18 +13,32 @@ import (
 	"github.com/AccelByte/sample-apps/pkg/repository"
 )
 
+type ClientCredentials struct {
+	ClientId     *string
+	ClientSecret *string
+}
+
 // clientLogin represents the client login command
 var clientLoginCmd = &cobra.Command{
 	Use:   "loginClient",
 	Short: "Login client",
 	Long:  `Login client via client_credentials flow`,
 	RunE: func(cmd *cobra.Command, args []string) error {
+		clientCredentials := ClientCredentials{}
+		if cmd.Flags().Changed("clientId") {
+			id := cmd.Flag("clientId").Value.String()
+			clientCredentials.ClientId = &id
+		}
+		if cmd.Flags().Changed("clientSecret") {
+			secret := cmd.Flag("clientSecret").Value.String()
+			clientCredentials.ClientSecret = &secret
+		}
 		oAuth20Service := iam.OAuth20Service{
 			Client:           factory.NewIamClient(&repository.ConfigRepositoryImpl{}),
 			ConfigRepository: &repository.ConfigRepositoryImpl{},
 			TokenRepository:  &repository.TokenRepositoryImpl{},
 		}
-		err := oAuth20Service.GrantTokenCredentials("", "")
+		err := oAuth20Service.LoginClient(clientCredentials.ClientId, clientCredentials.ClientSecret)
 		if err != nil {
 			return err
 		}
@@ -36,4 +50,6 @@ var clientLoginCmd = &cobra.Command{
 
 func init() {
 	RootCmd.AddCommand(clientLoginCmd)
+	clientLoginCmd.Flags().StringP("clientId", "u", "", "Client ID (username)")
+	clientLoginCmd.Flags().StringP("clientSecret", "p", "", "Client secret (password)")
 }

aed4200a737aaa1680b4a28ad178d137a7553c7c
ruli <ruli@accelbyte.net>
2022-12-01 08:46:04 +0700

    chore(test): move to one file

--- services-api/pkg/tests/sdk/http_retry_test.go
+++ services-api/pkg/tests/sdk/http_retry_test.go
@@ -292,3 +292,83 @@ func prepareToken(iam *iam.BansService) {
 	t, _ := iam.TokenRepository.GetToken()
 	_ = iam.TokenRepository.Store(*t)
 }
+
+func TestRetry_MockServerCustomOverride(t *testing.T) {
+	maxNumberOfRetries := uint(1)
+	roundTripper := MyRoundTripper{
+		Counter:      0,
+		RoundTripper: iamBansService.Client.Runtime.Transport,
+	}
+	tests := []struct {
+		name   string
+		params bans.AdminGetBansTypeV3Params
+		err    string
+	}{
+		{
+			name: "override the default retry with three custom attempt",
+			params: bans.AdminGetBansTypeV3Params{
+				RetryPolicy: &utils.Retry{
+					Transport: &roundTripper,
+					MaxTries:  maxNumberOfRetries,
+					Backoff:   utils.NewConstantBackoff(0),
+					RetryCodes: map[int]bool{
+						404: true,
+					},
+				},
+			},
+		},
+		{
+			name: "retry with constant backoff",
+			params: bans.AdminGetBansTypeV3Params{
+				RetryPolicy: &utils.Retry{
+					Transport: &roundTripper,
+					MaxTries:  maxNumberOfRetries,
+					Backoff:   utils.NewConstantBackoff(0),
+					RetryCodes: map[int]bool{
+						404: true,
+					},
+				},
+			},
+		},
+		{
+			name: "retry with exponential backoff",
+			params: bans.AdminGetBansTypeV3Params{
+				RetryPolicy: &utils.Retry{
+					Transport: &roundTripper,
+					MaxTries:  maxNumberOfRetries,
+					Backoff:   utils.NewExponentialBackoff(utils.StartBackoff, utils.MaxBackoff),
+					RetryCodes: map[int]bool{
+						404: true,
+					},
+				},
+			},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			err := configureMockServerOverwriteResponse(map[string]interface{}{
+				"enabled":   true,
+				"overwrite": true,
+				"status":    404,
+			})
+			if err != nil {
+				t.Skip("unable to configure mock server")
+			}
+
+			time.Sleep(1 * time.Second)
+
+			input := &bans.AdminGetBansTypeV3Params{
+				RetryPolicy: tt.params.RetryPolicy,
+			}
+			input.RetryPolicy = tt.params.RetryPolicy
+			_, err = iamBansService.AdminGetBansTypeV3Short(input)
+			assert.NotNil(t, err, "fail on purpose")
+			t.Logf("name: %s, count: %v", tt.name, roundTripper.Counter)
+		})
+	}
+	assert.Equal(t, 6, roundTripper.Counter)
+
+	// Clean up
+	err := resetMockServerOverwriteResponse()
+	assert.Nil(t, err)
+}

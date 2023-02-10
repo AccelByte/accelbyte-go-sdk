ccb0c4bfa953c9df250b49c30ed407a84569057b
Abdul Aris Rahmanudin <abdul.rahmanudin@accelbyte.net>
2023-02-10 08:49:10 +0700

    fix: revert changes for match2 test

--- services-api/pkg/tests/integration/match2_test.go
+++ services-api/pkg/tests/integration/match2_test.go
@@ -83,7 +83,7 @@ func TestIntegrationMatchPool(t *testing.T) {
 
 	// CASE Create a match rule set
 	inputCreateRule := &rule_sets.CreateRuleSetParams{
-		Body: &match2clientmodels.APIRuleSetPayload{
+		Body: &match2clientmodels.APIMatchRuleSet{
 			Data: &data,
 			Name: &ruleSetName,
 		},

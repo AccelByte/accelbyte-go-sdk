bbe9875f69de677ce51c5a47231cc28fa559868c
ruli sastra putri <ruli@accelbyte.net>
2023-12-14 09:26:32 +0700

    chore: validate extend namespace
    (cherry picked from commit 84ab7b689e881ce1b34eb16cc5954013a9832c71)

--- services-api/pkg/service/iam/auth_validator_test.go
+++ services-api/pkg/service/iam/auth_validator_test.go
@@ -6,8 +6,10 @@ package iam
 
 import (
 	"fmt"
+	"os"
 	"testing"
 
+	"github.com/AccelByte/accelbyte-go-sdk/iam-sdk/pkg/iamclient/o_auth2_0"
 	"github.com/AccelByte/accelbyte-go-sdk/services-api/pkg/factory"
 	"github.com/AccelByte/accelbyte-go-sdk/services-api/pkg/utils/auth"
 	"github.com/stretchr/testify/assert"
@@ -40,7 +42,7 @@ func TestTokenValidator_ValidateToken(t *testing.T) {
 		return
 	}
 
-	namespace := "accelbyte"
+	namespace := os.Getenv("AB_NAMESPACE")
 	resourceName := "MMV2GRPCSERVICE"
 	requiredPermission := Permission{
 		Action:   2,
@@ -61,4 +63,45 @@ func TestTokenValidator_ValidateToken(t *testing.T) {
 	// Assert
 	assert.Nil(t, err)
 	assert.Equal(t, claims.Namespace, namespace)
+}
+
+func TestTokenValidator_ValidateExtendNamespace(t *testing.T) {
+	// should be moved and run as integration test, skip for now
+	t.Skip()
+
+	// Arrange
+	configRepo := auth.DefaultConfigRepositoryImpl()
+	tokenRepo := auth.DefaultTokenRepositoryImpl()
+	authService := OAuth20Service{
+		Client:           factory.NewIamClient(configRepo),
+		ConfigRepository: configRepo,
+		TokenRepository:  tokenRepo,
+	}
+
+	extendNamespace := os.Getenv("AB_NAMESPACE")
+
+	token, err := authService.TokenGrantV3Short(&o_auth2_0.TokenGrantV3Params{
+		ExtendNamespace: &extendNamespace,
+		GrantType:       o_auth2_0.TokenGrantV3UrnIetfParamsOauthGrantTypeExtendClientCredentialsConstant,
+	})
+	if err != nil {
+		assert.Fail(t, err.Error())
+
+		return
+	}
+
+	authService.SetLocalValidation(false)                                                // true will do it locally, false will do it remotely
+	claims, errClaims := authService.ParseAccessTokenToClaims(*token.AccessToken, false) // false will not validate at all, only claim. This test will validate in the next line
+	if errClaims != nil {
+		assert.Fail(t, errClaims.Error())
+
+		return
+	}
+
+	// Act
+	err = authService.Validate(*token.AccessToken, nil, &extendNamespace, nil) // validate against the extend namespace
+
+	// Assert
+	assert.Nil(t, err)
+	assert.Equal(t, claims.ExtendNamespace, extendNamespace)
 }

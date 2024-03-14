ef2bcf9df926e3b7742e15ddb481da016f03dd54
ruli sastra putri <ruli@accelbyte.net>
2024-03-15 00:35:20 +0700

    test(core): websocket token refresh

--- services-api/pkg/tests/sdk/refreshToken_test.go
+++ services-api/pkg/tests/sdk/refreshToken_test.go
@@ -5,11 +5,19 @@
 package sdk_test
 
 import (
+	"bufio"
+	"strings"
 	"testing"
 	"time"
 
 	"github.com/AccelByte/accelbyte-go-sdk/iam-sdk/pkg/iamclient/bans"
+	"github.com/AccelByte/accelbyte-go-sdk/services-api/pkg/model"
 	"github.com/AccelByte/accelbyte-go-sdk/services-api/pkg/repository"
+	"github.com/AccelByte/accelbyte-go-sdk/services-api/pkg/service"
+	"github.com/AccelByte/accelbyte-go-sdk/services-api/pkg/tests/integration"
+	"github.com/AccelByte/accelbyte-go-sdk/services-api/pkg/utils/auth"
+	"github.com/AccelByte/accelbyte-go-sdk/services-api/pkg/utils/connectionutils"
+	"github.com/sirupsen/logrus"
 	"github.com/stretchr/testify/assert"
 )
 
@@ -139,4 +147,117 @@ func TestAuthInfoWriterRefreshAsync_withMockServer(t *testing.T) {
 			})
 		}
 	})
+}
+
+var (
+	connMgr             *integration.ConnectionManagerImpl
+	lobbyMessageHandler = func(dataByte []byte) {
+
+		msg := decodeWSMessage(string(dataByte))
+
+		if v, ok := msg["type"]; ok {
+			msgType = v
+		}
+		switch msgType {
+		case model.TypeNotificationMessage:
+		}
+	}
+	msgType string
+)
+
+func TestWebsocketRefresh_withMockServer(t *testing.T) {
+	// 1. request with valid Token
+	err := oAuth20Service.LoginUser(ConstUsername, ConstPassword)
+	if err != nil {
+		assert.FailNow(t, err.Error())
+	}
+	assert.Nil(t, err, "err should be nil")
+
+	tokenRepoObs := oAuth20Service.TokenRepository
+	configRepoObs := oAuth20Service.ConfigRepository
+
+	// 2. call websocket endpoint
+	lobbyService := &service.LobbyServiceWebsocket{
+		ConfigRepository:  configRepoObs,
+		TokenRepository:   tokenRepoObs,
+		ConnectionManager: connMgr,
+	}
+
+	connection, err := connectionutils.NewWebsocketConnection(configRepoObs, tokenRepoObs, lobbyMessageHandler)
+	assert.Nil(t, err, "err should be nil")
+
+	connMgr.Save(connection)
+
+	friendsId := "friendsId"
+	id := "test"
+	errNotif1 := lobbyService.ListOfFriendsRequest(&friendsId, &id)
+	assert.NoError(t, errNotif1)
+
+	// 3. get the Token from repository
+	getToken, errGetToken := oAuth20Service.TokenRepository.GetToken()
+	if errGetToken != nil {
+		assert.FailNow(t, errGetToken.Error())
+	}
+	Repository := oAuth20Service.GetAuthSession().Refresh
+	hasExpired := repository.HasTokenExpired(tokenRepoObs, Repository.GetRefreshRate())
+	assert.False(t, hasExpired) // Token not yet expired
+	secondsTillExpiry := repository.GetSecondsTillExpiry(tokenRepoObs, Repository.GetRefreshRate())
+	t.Logf("Expiring in... : %v", secondsTillExpiry)
+
+	// 4. force the Token to be expired
+	expiresIn = int32(5)
+	getToken.ExpiresIn = &expiresIn       // monkey-patch, force expiry Token
+	getToken.RefreshExpiresIn = expiresIn // monkey-patch, force expiry refreshToken
+
+	getExpiresIn, _ := repository.GetExpiresIn(tokenRepoObs)
+	assert.Equal(t, *getExpiresIn, expiresIn)
+	getRefreshRate := Repository.GetRefreshRate()
+	assert.NotNil(t, getRefreshRate)
+	secondsTillExpiry = repository.GetSecondsTillExpiry(tokenRepoObs, Repository.GetRefreshRate())
+	t.Logf("Force to expire in... : %v", secondsTillExpiry)
+
+	// Use the ObservableTokenRepositoryImpl
+	observableRepo := auth.DefaultObservableTokenRepositoryImpl(tokenRepoObs)
+
+	// Register lobbyService as an observer
+	observableRepo.RegisterObserver(lobbyService)
+
+	// Set access token and observe the change
+	newToken, errNewToken := tokenRepoObs.GetToken()
+	assert.NoError(t, errNewToken)
+
+	// store the new monkey patch Token
+	errSetAccessToken := observableRepo.SetAccessToken(newToken)
+	assert.NoError(t, errSetAccessToken)
+
+	time.Sleep(time.Duration(*getExpiresIn) * time.Second)
+	hasIndeedExpired := repository.HasTokenExpired(tokenRepoObs, Repository.GetRefreshRate())
+	assert.True(t, hasIndeedExpired) // indeed expired
+
+	// 5. call again with time sleep
+	errNotif2 := lobbyService.ListOfFriendsRequest(&friendsId, &id)
+	assert.NoError(t, errNotif2)
+	assert.False(t, hasExpired)
+
+	errLogout := oAuth20Service.Logout()
+	assert.Nil(t, errLogout)
+}
+
+func decodeWSMessage(msg string) map[string]string {
+	scanner := bufio.NewScanner(strings.NewReader(msg))
+	res := make(map[string]string)
+
+	for scanner.Scan() {
+		str := scanner.Text()
+		keyValue := strings.Split(str, ": ")
+		if len(keyValue) == 2 {
+			res[keyValue[0]] = keyValue[1]
+		}
+	}
+
+	if err := scanner.Err(); err != nil {
+		logrus.Errorf("error reading websocket message: %v", err)
+	}
+
+	return res
 }

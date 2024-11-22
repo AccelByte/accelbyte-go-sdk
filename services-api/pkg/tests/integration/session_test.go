fa7d8adeab5aea67aaf20dae34fd6e2de94faa51
ruli sastra putri <ruli@accelbyte.net>
2024-11-22 08:35:06 +0700

    chore(test): integration - remove code pointer in session service

--- services-api/pkg/tests/integration/session_test.go
+++ services-api/pkg/tests/integration/session_test.go
@@ -317,7 +317,7 @@ func TestIntegrationParty(t *testing.T) {
 
 	// CASE User join a party with code
 	inputJoined := &partySession.PublicPartyJoinCodeParams{
-		Body:      &sessionclientmodels.ApimodelsJoinByCodeRequest{Code: &created.Code},
+		Body:      &sessionclientmodels.ApimodelsJoinByCodeRequest{Code: created.Code},
 		Namespace: integration.NamespaceTest,
 	}
 	joined, errJoined := partyServiceFor2ndPlayer.PublicPartyJoinCodeShort(inputJoined)

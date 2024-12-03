2acf182fd2e7bacfa586680e261c7b912af2b224
ruli sastra putri <ruli@accelbyte.net>
2024-12-03 08:31:31 +0700

    chore(test): add upload and download helper function

--- services-api/pkg/tests/sdk/http_test.go
+++ services-api/pkg/tests/sdk/http_test.go
@@ -5,61 +5,119 @@
 package sdk_test
 
 import (
-	"bytes"
 	"io"
-	"io/ioutil"
-	"mime/multipart"
+	"net/http"
+	"net/http/httptest"
 	"os"
+	"strings"
 	"testing"
 
 	"github.com/AccelByte/accelbyte-go-sdk/services-api/pkg/utils"
 	"github.com/stretchr/testify/assert"
 )
 
-func TestUploadBinary(t *testing.T) {
-	token, err := oAuth20Service.TokenRepository.GetToken()
+// Helper function to set up a mock server
+func setupMockServer(t *testing.T, method string, content string) *httptest.Server {
+	t.Helper()
+
+	return httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		if r.Method != method {
+			t.Errorf("Expected '%s' request, got '%s'", method, r.Method)
+		}
+		if !strings.HasPrefix(r.Header.Get("Authorization"), "Bearer ") {
+			t.Errorf("Expected Authorization header to start with 'Bearer '")
+		}
+		if method == http.MethodPost {
+			_, err := io.ReadAll(r.Body)
+			if err != nil {
+				t.Errorf("Failed to read request body: %v", err)
+			}
+		}
+		_, err := w.Write([]byte(content))
+		if err != nil {
+			t.Errorf("Failed to write the content. %v", err)
+		}
+	}))
+}
+
+// Helper function to create a temporary file for binary upload
+func createTempFile(t *testing.T, content string) *os.File {
+	t.Helper()
+
+	file, err := os.CreateTemp("", "upload-test-*.bin")
 	if err != nil {
-		return
+		t.Fatalf("Failed to create temp file: %v", err)
 	}
-	url := mockServerBaseUrl + "/lobby/v1/admin/config/namespaces/test/import"
-
-	filePath := "test.bin"
-	err = os.WriteFile(filePath, []byte("test"), 0666)
+	_, err = file.Write([]byte(content))
 	if err != nil {
-		assert.Nil(t, err)
+		t.Fatalf("Failed to write to temp file: %v", err)
 	}
-
-	file, err := os.Open(filePath)
+	_, err = file.Seek(0, 0) // Reset file pointer to the beginning
 	if err != nil {
-		assert.Nil(t, err)
+		t.Fatalf("Failed to reset file pointer: %v", err)
 	}
 
-	var requestBody bytes.Buffer
-	w := multipart.NewWriter(&requestBody)
+	return file
+}
 
-	part, err := w.CreateFormFile("file", filePath)
-	assert.Nil(t, err)
+func TestUploadBinaryFile(t *testing.T) {
+	// Prepare
+	ts := setupMockServer(t, http.MethodPost, "")
+	defer ts.Close()
 
-	_, err = io.Copy(part, file)
-	assert.Nil(t, err)
-
-	// Arrange - cleaned up after done
+	file := createTempFile(t, "This is a test file content")
 	defer func() {
-		_ = os.Remove(filePath)
-		_ = w.Close()
+		_ = os.Remove(file.Name())
 	}()
 
-	resp, errResp := utils.SimpleHTTPCall(utils.GetClient(), url, "POST", "Bearer "+*token.AccessToken, "", &requestBody)
-	if errResp != nil {
-		assert.Nil(t, errResp)
+	// Act
+	resp, err := utils.UploadBinaryFile(ts.URL, "test-token", file.Name())
+	if err != nil {
+		t.Fatalf("UploadBinary failed: %v", err)
 	}
-	respBody, errRespBody := ioutil.ReadAll(resp.Body)
-	if errRespBody != nil {
-		assert.Nil(t, errRespBody)
+
+	// Assert
+	assert.Nil(t, err)
+	assert.NotNil(t, resp)
+	assert.Equal(t, resp.StatusCode, http.StatusOK)
+}
+
+func TestDownloadBinaryFile(t *testing.T) {
+	// Prepare
+	expectedContent := "This is a test file content"
+	ts := setupMockServer(t, http.MethodGet, expectedContent)
+	defer ts.Close()
+
+	file := createTempFile(t, "")
+	defer func() {
+		_ = os.Remove(file.Name())
+	}()
+
+	// Act
+	resp, err := utils.DownloadBinary(ts.URL, "test-token", file.Name())
+	if err != nil {
+		t.Fatalf("DownloadBinary failed: %v", err)
 	}
-	if errResp != nil {
-		assert.NotNil(t, errResp, "err response is expected. the endpoint only accept JSON file. "+errResp.Error())
+	defer func() {
+		_ = resp.Body.Close()
+	}()
+
+	if resp.StatusCode != http.StatusOK {
+		t.Errorf("Expected status code %d, got %d", http.StatusOK, resp.StatusCode)
 	}
-	t.Logf(string(respBody))
-	assert.NotNil(t, string(respBody))
+
+	_, err = io.Copy(file, resp.Body)
+	if err != nil {
+		t.Fatalf("file write error: %s", err.Error())
+	}
+
+	content, err := os.ReadFile(file.Name())
+	if err != nil {
+		t.Fatalf("Failed to read downloaded file: %v", err)
+	}
+
+	// Assert
+	assert.Nil(t, err)
+	assert.NotNil(t, content)
+	assert.Equal(t, string(content), expectedContent)
 }
